f74f94edea3c5a70e93cc0aca08a490e230139f3
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-01-22 00:52:50 -0300

    test: Add tests for the HLC initialization process

---
+++ test/stateful_hlc_initialize_test.dart
@@ -0,0 +1,90 @@
+import 'package:drift_offline_first/drift_offline_first.dart';
+import 'package:drift_offline_first/src/hlc/stateful.dart';
+import 'package:test/test.dart';
+
+import 'utils/crdt_context.dart';
+import 'utils/database.dart';
+import 'utils/executor.dart';
+
+void main() {
+  group(
+      'Given a database opened with a CRDT control table having initial HLC from same user and node',
+      () {
+    late Hlc initialHlc;
+
+    setUp(() async {
+      initialHlc = Hlc.now(testNodeId);
+      await testDatabaseFixture(initialHlc: initialHlc);
+    });
+
+    test(
+        'when retrieving the cached HLC '
+        'then the HLC for the node is the initial HLC.', () async {
+      expect(StatefulHlc.cached(testUserId, testNodeId).lastHlc, equals(initialHlc));
+    });
+  });
+
+  group(
+      'Given a database opened with a CRDT control table having initial HLC from same user and different node',
+      () {
+    late Hlc initialHlc;
+
+    setUp(() async {
+      initialHlc = Hlc.now('other_node_id');
+      await testDatabaseFixture(initialHlc: initialHlc);
+    });
+
+    test(
+        'when retrieving the cached HLC '
+        'then the HLC for the node is the initial HLC for the requested node.',
+        () async {
+      final expectedHlc = initialHlc.apply(nodeId: testNodeId);
+      expect(StatefulHlc.cached(testUserId, testNodeId).lastHlc, equals(expectedHlc));
+    });
+  });
+
+  group(
+      'Given a database opened with a CRDT control table having initial HLC from different user and node',
+      () {
+    late Hlc initialHlc;
+
+    setUp(() async {
+      initialHlc = Hlc.now('other_node_id');
+      await testDatabaseFixture(initialHlc: initialHlc);
+    });
+
+    test(
+        'when retrieving the cached HLC '
+        'then the HLC for the node is zero for the requested node.', () async {
+      final expectedHlc = Hlc.zero(testNodeId);
+      expect(
+          StatefulHlc.cached('other_user_id', testNodeId).lastHlc, equals(expectedHlc));
+    });
+  });
+}
+
+Future<TodoDb> testDatabaseFixture({
+  required Hlc initialHlc,
+  String? userId,
+}) async {
+  setTestFileExecutor();
+  final database = TodoDb(testExecutor);
+
+  final (crdt, crdtDb, nodeId) = database.crdtContext;
+
+  await crdtDb.managers.crdtDataTable.create(
+    (t) => t(
+      userId: userId ?? crdt.userId,
+      tblName: 'todos',
+      columnName: 'content',
+      rowId: '1',
+      hlcTimestamp: initialHlc,
+    ),
+  );
+
+  await database.close();
+  await testExecutor.close();
+
+  setTestFileExecutor();
+  return TodoDb(testExecutor);
+}
